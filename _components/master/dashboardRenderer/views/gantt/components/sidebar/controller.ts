be9ad73a7b37e0a612f56b1aa94f81f517a86e4b
Sneider Narvaez <sneider.developer@gmail.com>
2026-08-11 10:24:43 -0500

    Add Gantt chart sidebar controller with computed properties and methods

---
+++ _components/master/dashboardRenderer/views/gantt/components/sidebar/controller.ts
@@ -0,0 +1,25 @@
+import { computed, toRefs } from 'vue'
+import { i18n } from 'src/plugins/utils.ts'
+import { TimelineRow } from '../../interface'
+import { getDuration } from '../../helper'
+
+export default function controller(props: any, emit: any) {
+
+  const { labels } = toRefs(props)
+
+  const computeds = {
+    nameLabel: computed(() => labels.value?.name || i18n.tr('isite.cms.form.name')),
+    durationLabel: computed(() => (
+      labels.value?.duration || i18n.tr('isite.cms.label.duration')
+    )),
+  }
+
+  const methods = {
+    duration: (row: TimelineRow) => getDuration(row.startAt, row.endAt),
+    rowHeight: (row: TimelineRow) => ({
+      height: `calc(var(--gantt-row-height) * ${row.subRows})`,
+    }),
+  }
+
+  return { ...computeds, ...methods }
+}
